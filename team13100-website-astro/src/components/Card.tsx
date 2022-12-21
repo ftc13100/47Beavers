ed372989248b00c9bf997eaf6482898e2340c95c
SachetK <71898487+SachetK@users.noreply.github.com>
2022-12-21 10:38:09 -0500

    Update to a more responsive design

--- team13100-website-astro/src/components/Card.tsx
+++ team13100-website-astro/src/components/Card.tsx
@@ -6,11 +6,16 @@ interface Props {
 
 const MoreInfoCard = ({ title, children, link }: Props) => {
   return (
-    <div className="bg-blue-800 w-1/6 h-3/5 m-6">
-      <div className="flex flex-col h-full items-center justify-around px-8">
-        <h1 className="md:text-4xl text-2xl font-bold text-white pt-6">{title}</h1>
+    <div className="bg-blue-800 w-1/6 h-2/3 2xl:3/5 m-6">
+      <div className="flex flex-col h-full items-center justify-evenly px-8">
+        <h1 className="2xl:text-4xl text-2xl font-bold text-white pt-6 text-center mb-2">
+          {title}
+        </h1>
         {children}
-        <a href={link} className="text-white">
+        <a
+          href={link}
+          className="text-white bg-blue-400 text-xs hover:bg-blue-600 py-2 px-4 m-4 2xl:py-4 2xl:px-8 2xl:m-4"
+        >
           More Info
         </a>
       </div>
--- team13100-website-astro/src/components/Header.tsx
+++ team13100-website-astro/src/components/Header.tsx
@@ -6,32 +6,37 @@ const Header = () => {
           <h1 className="font-bold text-2xl">FTC Team #13100</h1>
           <h1 className="font-bold text-2xl">47 Beavers</h1>
         </a>
-          <nav className="text-white flex flex-wrap items-start py-4 space-x-8">
-            <a href="/about" className="hover:text-white hover:underline">
-              History
+        <nav className="text-white text-xl flex flex-wrap items-start py-4 space-x-8">
+          <a href="/about" className="hover:text-white hover:underline">
+            History
+          </a>
+          <a href="/members" className="hover:text-white hover:underline">
+            Members
+          </a>
+          <div className="group flex flex-none flex-col items-center flex-wrap">
+            <a href="/camps" className="hover:text-white hover:underline">
+              {" "}
+              Camps{" "}
             </a>
-            <a href="/members" className="hover:text-white hover:underline">
-              Members
-            </a>
-            <div className="group flex flex-none flex-col items-center flex-wrap">
-              <a
-                href="/camps"
-                className="hover:text-white hover:underline"
-              >
-                {" "}
-                Camps{" "}
-              </a>
-              <div className="flex">
-                <div className="hidden group-hover:flex absolute right-8 flex-col space-y-2 items-center first:pt-4 last:pb-2 px-4 bg-blue-800">
-                  <a href="/camps/robotics" className="hover:underline">Robotics Design Camps</a>
-                  <a href="/camps/mathematics" className="hover:underline">Mathematics Camps</a>
-                  <a href="/camps/physics" className="hover:underline">Programming Camps</a>
-                  <a href="/camps/programming" className="hover:underline">Physics Camps</a>
-                </div>
+            <div className="flex">
+              <div className="hidden group-hover:flex absolute right-8 flex-col space-y-2 items-center first:pt-4 last:pb-2 px-4 bg-blue-800">
+                <a href="/camps/robotics" className="hover:underline">
+                  Robotics Design Camps
+                </a>
+                <a href="/camps/mathematics" className="hover:underline">
+                  Mathematics Camps
+                </a>
+                <a href="/camps/physics" className="hover:underline">
+                  Programming Camps
+                </a>
+                <a href="/camps/programming" className="hover:underline">
+                  Physics Camps
+                </a>
               </div>
             </div>
-          </nav>
-        </div>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
